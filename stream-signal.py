d564231df9a9258fed432a62d882284e97868214
AMihneaa <asmihnea5@gmail.com>
2026-03-27 19:26:51 +0200

    Raw Data Filter

--- stream-signal.py
+++ stream-signal.py
@@ -2,7 +2,6 @@ import asyncio
 import logging
 
 from myo import MyoClient
-
 from myo.types import (
     ClassifierEvent,
     AggregatedData,
@@ -10,49 +9,64 @@ from myo.types import (
     FVData,
     IMUData,
     EMGData,
-    MotionEvent
+    MotionEvent,
+    ClassifierMode,
+    EMGMode,
+    IMUMode,
 )
 
 addr = "E4:96:A9:A7:5C:74"
-
-logger= logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 class TestClient(MyoClient):
     async def on_classifier_event(self, ce: ClassifierEvent):
-        return await super().on_classifier_event(ce)
-    
+        pass
+
     async def on_aggregated_data(self, ad: AggregatedData):
-        return await super().on_aggregated_data(ad)
+        '''
+        0-7:  FVData
+        8-11: oRIENTATION qUATERNION
+        12-14: Accelerometru
+        15-17: Gyro
+        '''
+        print("AGG:", ad)
 
     async def on_emg_data_aggregated(self, eds: EMGDataSingle):
-        return await super().on_emg_data_aggregated(eds)
-    
+        print("EMG single:", eds)
+
     async def on_emg_data(self, data: EMGData):
-        return await super().on_data(data)
-    
+        print("EMG:", data)
+
     async def on_motion_event(self, me: MotionEvent):
-        return await super().on_motion_event(me)
-    
+        print("MOTION:", me)
+
     async def on_fv_data(self, fvd: FVData):
-        return await super().on_fv_data(fvd)
-    
+        print("FV:", fvd)
+
     async def on_imu_data(self, imu: IMUData):
-        return await super().on_imu_data(imu)
+        print("IMU:", imu)
 
 async def main(address):
-    client= await TestClient.with_device(addr, aggregate_all= True)
+    client = await TestClient.with_device(address, aggregate_all=False, aggregate_emg= True)
 
     try:
-        info= await client.get_services()
+        info = await client.get_services()
         logger.info(info)
 
+        await client.setup(
+            classifier_mode=ClassifierMode.DISABLED,
+            emg_mode=EMGMode.SEND_RAW,
+            imu_mode=IMUMode.SEND_ALL,
+        )
+
+        await client.start()
+        await asyncio.sleep(8)
+        await client.stop()
+        await client.disconnect()
 
     except Exception as e:
-        print(f'Error: {e}')
-
+        logger.exception("Error while connecting/streaming: %s", e)
 
 if __name__ == "__main__":
-    logging.basicConfig(level= logging.INFO)
-    logger.info(f' -- Log --')
-
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(main(addr))
